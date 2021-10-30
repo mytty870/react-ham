d5f457ce6c7cc77c3420abe0baa6af24b9ee0d5f
github mytty870 <otani11ace@icloud.com>
2021-10-30 21:28:28 +0900

    impletent component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,63 +1,31 @@
 import { useState } from "react";
 
 
-export const App = () => {
-  const [count, setCount] = useState(0)
+export const App = props => {
+  
 
-  const increment = () => {
-    setCount(count + 1)
-  }
-
-  const decrement = () => {
-    if (count > 0) {
-      setCount(count - 1)
-    }
-  }
-
-  const increment2 = () => {
-    setCount(prev => prev + 1)
-  }
-  const decrement2 = () => {
-    setCount(prev => prev - 1)
-  }
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
   const reset = () => {
-    setCount(0)
-  }
-
-  const duble = () => {
-    setCount(prev => prev * 2)
-  }
-
-  const san = () => {
-    if (count % 3 === 0) {
-      setCount(count / 3)
-    }
-
+    setPrice(props.price)
+    setName(props.name)
   }
 
   return (
     <>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={duble}>x2</button>
-        <button onClick={san}>3の倍数だけ3でわる</button>
-      </div>
+      <p>現在の{name}は、{price}です</p>
+      <button onClick={() => setPrice(prev => prev + 1)}>+1</button>
+      <button onClick={() => setPrice(prev => prev - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 
