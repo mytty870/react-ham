570fed04db0ee7b6428d8f0a960b7ad714bd1d9f
github mytty870 <otani11ace@icloud.com>
2021-10-30 21:47:47 +0900

    impletent object state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import { useState } from "react";
 
 export const App = props => {
   
-
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
-
+  const [state, setState] = useState(props);
+  
+  const { name, price } = state
   return (
     <>
       <p>現在の{name}は、{price}です</p>
-      <button onClick={() => setPrice(prev => prev + 1)}>+1</button>
-      <button onClick={() => setPrice(prev => prev - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})} />
     </>
   );
-}
+};
 
 App.defaultProps = {
   name: '',
