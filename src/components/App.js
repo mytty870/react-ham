13b7b6dd31dbde7dc1d67567604f608fe37a8783
github mytty870 <otani11ace@icloud.com>
2021-10-31 15:15:53 +0900

    events

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useReducer, useState } from 'react';
+import { useReducer } from 'react';
 import { events as reducer } from "../reducers"
-import { Event } from './Event';
+import { EventForm } from './EventForm'
+import { Events } from './Events';
 
 export const App = () => {
 
   const [state, dispatch] = useReducer(reducer, [])
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-
-  const addEvent = (e) => {
-    e.preventDefault()
-    dispatch(
-      {
-        type: 'CREATE_EVENT',
-        title,
-        body
-      }
-    )
-    setTitle('')
-    setBody('')
-  }
-
-  const deleteAllEvents = e => {
-    e.preventDefault();
-    const result = window.confirm('全てのイベントを本当に削除してもいいですか?')
-    if (result) dispatch({ type: 'DELETE_ALL_EVENTS'})
-  }
   
-  const unCreatable = title === '' || body === ''
-  
-
   return (
     <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={title} 
-          onChange={e => setTitle(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" value={body}
-          onChange={e => setBody(e.target.value)} />
-        </div>
+      <EventForm state={state} dispatch={dispatch} />
+      <Events />
 
-        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length
-         === 0}>全てのイベントを削除する</button>
-      </form>
-
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          { state.map((e, index) => (<Event key={index} e={e} dispatch={dispatch} />))}
-          
-        </tbody>
-      </table>
+      
     </div>
   );
 }
---
+++ src/components/Events.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Event } from './Event'
+
+export const Events = (props) => {
+  const {state, dispatch} = props;
+  return (
+    <>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          { state.map((e, index) => (<Event key={index} e={e} dispatch={dispatch} />))}
+
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+
+      
+
+
